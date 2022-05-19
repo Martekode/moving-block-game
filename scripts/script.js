b3934d39187f14852212fa3d5cd4cbb79cc7e49b
Marteko9000 <themarteko@protonmail.com>
2022-05-19 11:52:43 +0200

    added deadzone checking with alert on death

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,19 @@ var mousePosDisplay = document.getElementById('mousePos');
 var player = document.getElementById('player');
 var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
+const border =15;
+const radius = 15;
 document.addEventListener('mousemove',function(mouse){
     mousePosDisplay.innerText = `x: ${mouse.x} y: ${mouse.y}`;
     player.style.transform = 'translateY('+(mouse.y)+'px)';
     player.style.transform += 'translateX('+(mouse.x)+'px)';
-    /*if (player.style.top <= ){
+    //border x + half player width
+    if (mouse.x <= radius+border || mouse.x>=(windowWidth-border)-radius){
         alert('you loose');
-    }*/
-    
+    }
+    else if(mouse.y <= radius+border || mouse.y >= (windowHeight -border)-radius){
+        alert('you loose');
+    }
     
     
     
