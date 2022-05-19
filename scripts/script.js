4af67d5b0b972e2d86cb1eb16c2c5ff5405626b0
Marteko9000 <themarteko@protonmail.com>
2022-05-19 11:27:27 +0200

    border fixed of playZone

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
 var mousePosDisplay = document.getElementById('mousePos');
 var player = document.getElementById('player');
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
 document.addEventListener('mousemove',function(mouse){
     mousePosDisplay.innerText = `x: ${mouse.x} y: ${mouse.y}`;
     player.style.transform = 'translateY('+(mouse.y)+'px)';
     player.style.transform += 'translateX('+(mouse.x)+'px)';
+    /*if (player.style.top <= ){
+        alert('you loose');
+    }*/
+    
+    
     
     
 })
